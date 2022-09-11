8470fc706c5b3d7aebc7d5bb0614652ab54b3d01
Victor <victorribeiro172@gmail.com>
2022-09-11 10:56:30 -0300

    #1 Uso das funções execv e kill

---
+++ JoaoII.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(){
+    int tempo = 1;
+    
+    
+    printf("\n--------JOAO II O SEGUNDO CRIADOR DO MUNDO LINCH--------\n\n");
+    // Inicio João II
+    printf("João II: Sou João II o segundo criador do mundo LINCH, este é meu PID %d\n", getpid());
+    sleep(tempo);
+    printf("João II: Não vou cometer os mesmos erros do meu pai.\n");
+    sleep(tempo);
+    
+    return 0;
+}
